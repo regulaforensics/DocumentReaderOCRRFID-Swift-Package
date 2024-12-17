f82395b7bbc69c7823936c5f34a2fa17ba759c10
Administrator Regula <administratorregula@xcode.regula.local>
2024-12-17 13:03:33 +0300

    Release 7.5.10933

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["OCRRFID"]),
     ],
     targets: [
-        .binaryTarget(name: "OCRRFID", url: "https://pods.regulaforensics.com/OCRRFID/7.4.9827/DocumentReaderCore_ocrandmrzrfid_7.4.9827.zip", checksum: "9391435ce0feb6294c7af969bc4f1f43f7baf5dbe5eca0d605f24ec0b6f280bc"),
+        .binaryTarget(name: "OCRRFID", url: "https://pods.regulaforensics.com/OCRRFID/7.5.10933/DocumentReaderCore_ocrandmrzrfid_7.5.10933.zip", checksum: "7a975796bd3465111da42db8e8e3225247ede13684d336473f0aa6a4d1fbd6d2"),
     ]
 )
