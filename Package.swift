9e26643d6168dee19e8249af2bc8e1f8beb817f1
jenkins <jenkins@regula.by>
2024-12-26 16:19:58 +0300

    Release 7.5.10995

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["OCRRFID"]),
     ],
     targets: [
-        .binaryTarget(name: "OCRRFID", url: "https://pods.regulaforensics.com/OCRRFID/7.5.10933/DocumentReaderCore_ocrandmrzrfid_7.5.10933.zip", checksum: "7a975796bd3465111da42db8e8e3225247ede13684d336473f0aa6a4d1fbd6d2"),
+        .binaryTarget(name: "OCRRFID", url: "https://pods.regulaforensics.com/OCRRFID/7.5.10995/DocumentReaderCore_ocrandmrzrfid_7.5.10995.zip", checksum: "caf480e07d45c4c1d884e75cb11c6b516f0216db2c04faa7d3bf39b199db51c9"),
     ]
 )
